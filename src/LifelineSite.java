4e0489de9177727e740bf2c3f6a40fe30e58642f
ajanb <ajanbalaganesh@cmail.carleton.ca>
2025-11-07 12:12:11 -0500

    Extracted getBase()

--- src/LifelineSite.java
+++ src/LifelineSite.java
@@ -4,9 +4,14 @@ public class LifelineSite extends Site {
     public LifelineSite(int units, double rate) { super(units, rate); }
 
     public double getBillableAmount() {
-        double base = units * rate;
+        double base = getBase();
         double tax  = base * Site.TAX_RATE;
         return base + tax;
+    }
+
+    private double getBase() {
+        double base = units * rate;
+        return base;
     }
 }
 
